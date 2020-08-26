eb1f912cb9995b3cf436deb28c748422d95774ad
joriesieck <jsieck@stevens.edu>
2020-08-26 11:51:46 -0400

    changed setup to use material-table instead of the material-ui table, new download structure

--- src/Reports.js
+++ src/Reports.js
@@ -1,100 +1,170 @@
 const { Component } = wp.element;
 import Button from '@material-ui/core/Button';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
+import ReactHtmlParser from 'react-html-parser';
+import MaterialTable from "material-table";
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+  };
 
 class Reports extends Component {
-	/*
-	 * takes in an object and returns it as an array
+	/**
+	 * takes in a row and formats it into an object with k=>v pairs for each cell
+	 * called in genRows()
 	*/
-	genArray = (dataObj) => {
-		const dataArray = [];
-		const keys = Object.keys(dataObj);
-		keys.forEach((key) => {
-			dataArray.push(dataObj[key]);
-		})
-		return dataArray;
-	}
-
-	/*
-	 * generates the subtitle row
-	*/
-	genSubtitle = (nCols) => {
-		let rows = []
-		for(let i=0;i<nCols;i++) {
-			if(i>3) {
-				if(i%2) {
-					rows.push("Attempts")
-				} else {
-					rows.push("Score")
-				}
+	formatData = (row) => {
+		let formattedRow = {};
+		const keys = Object.keys(row);
+		keys.map((key) => {
+			const cell = row[key];
+			if(typeof(cell) != 'object') {
+				formattedRow[key] = cell;
 			} else {
-				rows.push("")
-			}
-		}
-		return rows;
-	}
-
-	/*
-	 * takes in an array and an object, flattens the object, and adds it to the array
-	*/
-	collapseScores = (rowArray,scoresObj) => {
-		const keys = Object.keys(scoresObj);
-		console.log(keys);
-		keys.forEach((key,i) => {
-			if(i>0) {
-				rowArray.push(scoresObj[key]['score']);
-				rowArray.push(scoresObj[key]['attempts']);
+				formattedRow[key] = ReactHtmlParser(`<div class="sub"><span>${cell['score']}</span><span>${cell['attempts']}</span></div>`);
 			}
 		});
-		// essentially move the final challenge key to the end
-		rowArray.push(scoresObj[keys[0]]['score']);
-		rowArray.push(scoresObj[keys[0]]['attempts']);
-		return rowArray;
+		return formattedRow;
 	}
 
 	/*
-	 * calls genArray() and collapseScores(), then generates a row from the new array
+	 * generates an array of row objects where each object has a key=>value pair with the data for each column
+	 * called in render()
 	*/
-	genRow = (rowObj) => {
-		let rowArray = this.genArray(rowObj);	// generate an array of the row data
-		const scoresObj = rowArray[4];	// get the scores object
-		rowArray.splice(4,1);	// remove the scores object from the array
-		rowArray = this.collapseScores(rowArray,scoresObj);	// collapse the scores object and add it to the array
-		return rowArray.map((cell,i) => i==3 ? <TableCell>{cell}%</TableCell> : <TableCell>{cell}</TableCell>);
+	genRows = () => {
+		let data = [];
+		const usersData = repObj.usersData;
+		const keys = Object.keys(usersData);
+		keys.map((key) => {
+			data.push(this.formatData(usersData[key]));
+		})
+		return data;
+	}
+
+	/*
+	 * generates an array of column objects where each object has a title and field
+	 * "title" = column title, "field" = column id (matches the tag in userData)
+	 * called in render()
+	*/
+	genColumns = () => {
+		let columns = [];
+		const headers = repObj.sectionHeaders;
+		const titles = Object.keys(headers);
+		titles.map((title,i) => {
+			let titleObj = {};
+			if(i>3) {
+				titleObj = {title:ReactHtmlParser(`${title}<div class="sub"><span>Score</span><span>Attempts</span></div>`),field:headers[title].toString()};
+			} else {
+				titleObj = {title,field:headers[title].toString()};
+			}
+			
+			columns.push(titleObj);
+		});
+		return columns;
+	}
+
+	/**
+	 * preps the data & opens a download dialogue to save as CSV
+	 * called in render() when the csv download button is clicked
+	*/
+	handleDownload() {
+		const columns = Object.keys(repObj.sectionHeaders);
+		const data = repObj.usersData;
+
+		const filename = 'Study Skills Report.csv';
+		let content = '';
+
+		// format columns and add to content string -> each column is a cell in one row
+		columns.forEach((column,i) => {
+			// the score/attempts columns are all arrays, the others are just strings
+			if(i>3) {
+				content += `${column} Score,${column} Attempts${column==='Final Challenge' ? '\n' : ','}`;
+			} else {
+				content += column + ',';
+			}
+		});
+
+		// format data and add to content string -> each element in data is a row
+		const userIDs = Object.keys(data);	// 1 row = 1 user
+		userIDs.forEach((id) => {
+			const row = data[id];
+			const keys = Object.keys(row);	// 0 final, 1-5 scores, 6-9 username etc (in correct order)
+			// 'normal' cells
+			for(let i=6;i<10;i++) {
+				const cell = row[keys[i]].toString();
+				console.log(typeof(cell));
+				content += cell.replace(/,/g, '') + ',';	// escape any commas
+			}
+			for(let i=1;i<6;i++) {
+				const dataPair = row[keys[i]];
+				content += `${dataPair.score},${dataPair.attempts},`;
+			}
+			const finalChallenge = row[keys[0]];
+			content += `${finalChallenge.score},${finalChallenge.attempts}\n`;
+		})
+
+		// prepare & download file
+		var element = document.createElement('a');
+		element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(content));
+		element.setAttribute('download', filename);
+		element.style.display = 'none';
+		document.body.appendChild(element);
+		element.click();
+		document.body.removeChild(element);
 	}
 
 	render() {
-		const userArray = this.genArray(repObj.usersData);	// generate an array of all the users
+		const columns = this.genColumns();
+		const data = this.genRows();
+
 		return (
 			<div>
-				<Table size="small">
-					<TableHead>
-						<TableRow>
-							{repObj.sectionHeaders.map((header,i) => {
-								if(i>3) {
-									return <TableCell colSpan={2}>{header}</TableCell>
-								}
-								return <TableCell>{header}</TableCell>
-							})}
-						</TableRow>
-						<TableRow>
-							{this.genSubtitle(16).map((subtitle) => (
-								<TableCell>{subtitle}</TableCell>
-							))}
-						</TableRow>
-					</TableHead>
-					<TableBody>
-						{userArray.map((user) => (
-							<TableRow>
-								{this.genRow(user)}
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
+			<MaterialTable
+				columns={columns}
+				data={data}
+				title={<Button className="ta-home-button" href={repObj.courseUrl}><span class="dashicons dashicons-admin-home"></span></Button>}
+				options={{
+					exportButton:true,
+					exportFileName:"Study Skills Report",
+					exportCsv: (columns, data) => this.handleDownload(),
+					fixedColumns: {
+		        left: 1,
+		        right: 0
+		      }
+				}}
+				icons={tableIcons}
+			/>
 			</div>
 		);
 	}
--- ta-reports.php
+++ ta-reports.php
@@ -13,39 +13,14 @@ if(!function_exists('register_block_type')) {
 	return;
 }
 
-global $ta_report_csv_headers;
-$ta_report_csv_headers = array(
-	"Username",
-	"Name",
-	"Last Login",
-	"Percent Complete",
-	"Memory Quiz Score",
-	"Memory Quiz Attempts",
-	"Technical Reading Quiz Score",
-	"Technical Reading Quiz Attempts",
-	"Taking Notes Quiz Score",
-	"Taking Notes Quiz Attempts",
-	"Time Management Quiz Score",
-	"Time Management Quiz Attempts",
-	"Test Taking Quiz Score",
-	"Test Taking Quiz Attempts",
-	"Final Challenge Score",
-	"Final Challenge Attempts"
-);
-
-/*
- * button to download as csv
-*/
-function ta_generate_download_button() {
-	?>
-	<a href="<?php echo esc_url( admin_url( 'admin-ajax.php' ) . '?action=ta_download_report_csv' ); ?>" class="ta-download-button"><?php esc_html_e( 'Download as CSV', 'arc-jquery-ajax' ); ?></a>
-	<?php
-}
-
 /*
  * generate the user data for a given user
 */
 function ta_generate_user_data($course_id,$total_course_steps,$user,$user_id,$quiz_data,$enrolled = false) {
+	$user_data = $quiz_data;
+	$user_data["username"] = $user->user_login;
+	$user_data["name"] = $user->display_name;
+
 	// get last login time from pmpro
 	$last_login_time = get_user_meta($user_id,'pmpro_logins',true)['last'];
 
@@ -56,29 +31,19 @@ function ta_generate_user_data($course_id,$total_course_steps,$user,$user_id,$qu
 
 		// for each quiz, log the most recent score & number of attempts
 		$quizzes_completed = learndash_get_user_quiz_attempt($user_id,$course_id);
+
 		$quiz_keys = array_keys($quiz_data);
 		foreach($quizzes_completed as $quiz) {
 			$quiz_id = $quiz['quiz'];
 			if(in_array($quiz_id,$quiz_keys)) {
-				$quiz_data[$quiz_id]['score'] = $quiz['percentage'];
-				$quiz_data[$quiz_id]['attempts'] += 1;
+				$user_data[$quiz_id]['score'] = $quiz['percentage'];
+				$user_data[$quiz_id]['attempts'] += 1;
 			}
 		}
 	}
 
-	// sort the data by quiz id (assumes quizzes were created in order)
-	// krsort($quiz_data);
-	//
-	// d($quiz_data);
-
-	// organize the data for easy access in js
-	$user_data = array(
-		"username" => $user->user_login,
-		"name" => $user->display_name,
-		"lastLogin" => $last_login_time ? $last_login_time : 'No recorded login time',
-		"percentComplete" => $percent_complete ? $percent_complete : 0,
-		"quizData" => $quiz_data
-	);
+	$user_data["lastlogin"] = $last_login_time ? $last_login_time : 'None';
+	$user_data["percentcomplete"] = $percent_complete ? $percent_complete : 0;
 
 	return $user_data;
 }
@@ -86,27 +51,12 @@ function ta_generate_user_data($course_id,$total_course_steps,$user,$user_id,$qu
 /*
  * generate user data
 */
-function ta_reports_get_user_data($user,$user_id,$course_id) {
+function ta_reports_get_user_data($user,$user_id,$course_id,$quiz_data) {
+	if(!$quiz_data) {
+		$quiz_data = ta_get_quizzes();
+	}
 	// get total course steps
 	$total_course_steps = learndash_get_course_steps_count($course_id);
-
-	// get list of quizzes
-	global $wpdb;
-	$post_table_name = $wpdb->prefix . 'posts';
-	// $sql = "SELECT `ID` FROM `{$post_table_name}` WHERE (`post_title` = 'Section Quiz' OR `post_title` = 'Final Challenge') AND `post_type` = 'sfwd-quiz' AND  `post_status` =  'publish'";
-	$sql = "SELECT `ID` FROM `{$post_table_name}` WHERE (`post_title` = 'Section Quiz') AND `post_type` = 'sfwd-quiz' AND  `post_status` =  'publish'";
-	$quiz_ids = $wpdb->get_results($sql, 'ARRAY_N');
-	// add final challenge at end
-	$sql = "SELECT `ID` FROM `{$post_table_name}` WHERE (`post_title` = 'Final Challenge') AND `post_type` = 'sfwd-quiz' AND  `post_status` =  'publish'";
-	$quiz_ids = array_merge($quiz_ids,$wpdb->get_results($sql, 'ARRAY_N'));
-
-	// fill in with dummy data
-	$quiz_data = array();
-	foreach($quiz_ids as $quiz) {
-		$quiz_id = $quiz[0];
-		$quiz_data[$quiz_id]['score'] = 'N/A';
-		$quiz_data[$quiz_id]['attempts'] = 0;
-	}
 
 	// check for sponsored members & get list
 	$sponsored_members = pmprosm_getChildren($user_id);
@@ -122,6 +72,29 @@ function ta_reports_get_user_data($user,$user_id,$course_id) {
 	}
 
 	return $users_data;
+}
+
+function ta_get_quizzes() {
+	// get list of quizzes
+	global $wpdb;
+	$post_table_name = $wpdb->prefix . 'posts';
+	$sql = "SELECT `ID` FROM `{$post_table_name}` WHERE (`post_title` = 'Section Quiz') AND `post_type` = 'sfwd-quiz' AND  `post_status` =  'publish'";
+	$quiz_ids = $wpdb->get_results($sql, 'ARRAY_N');
+	// add final challenge at end
+	$sql = "SELECT `ID` FROM `{$post_table_name}` WHERE (`post_title` = 'Final Challenge') AND `post_type` = 'sfwd-quiz' AND  `post_status` =  'publish'";
+	$quiz_ids = array_merge($quiz_ids,$wpdb->get_results($sql, 'ARRAY_N'));
+
+	// fill in with dummy data
+	$quiz_data = array();
+	foreach($quiz_ids as $quiz) {
+		$quiz_id = $quiz[0];
+		$quiz_data[$quiz_id] = array(
+			"score" => "N/A",
+			"attempts" => 0
+		);
+	}
+
+	return $quiz_data;
 }
 
 /*
@@ -153,25 +126,30 @@ function ta_get_report_data() {
 				$course_id = 4192;
 			}
 			$course_sections = learndash_30_get_course_sections($course_id);
+			$quiz_data = ta_get_quizzes();
+			$quiz_ids = array_keys($quiz_data);
 
 			$section_titles = array(
-				"Username",
-				"Name",
-				"Last Login",
-				"Percent Complete"
+				"Username" => "username",
+				"Name" => "name",
+				"Last Login" => "lastlogin",
+				"Percent Complete" => "percentcomplete"
 			);
+			$i = 0;
 			foreach ($course_sections as $section) {
-				array_push($section_titles,$section->post_title);
+				$section_titles[$section->post_title] = $quiz_ids[$i];
+				$i += 1;
 			}
-			array_push($section_titles,"Final Challenge");	// final challenge goes at the end
+			$section_titles["Final Challenge"] = $quiz_ids[$i];	// final challenge goes at the end
 
 			// generate user data
-			$users_data = ta_reports_get_user_data($user,$user_id,$course_id);
+			$users_data = ta_reports_get_user_data($user,$user_id,$course_id,$quiz_data);
 
 			// prep data
 			$report_data = array(
 				"sectionHeaders" => $section_titles,
 				"usersData" => $users_data,
+				"courseUrl" => learndash_get_course_url($course_id),
 			);
 
 			// send data to main.js
@@ -197,123 +175,5 @@ function ta_report_enqueue_styles() {
 
 }
 add_action( 'wp_enqueue_scripts', 'ta_report_enqueue_styles' );
-
-/*
- * function to actually handle the download - called in ta_download_report_csv()
-*/
-function ta_send_report_data($csv_file,$filename,$headers) {
-		// close temp file
-		fclose($csv_file);
-
-		if (version_compare(phpversion(), '5.3.0', '>')) {
-				//make sure we get the right file size
-				clearstatcache( true, $filename );
-		} else {
-				// for any PHP version prior to v5.3.0
-				clearstatcache();
-		}
-
-		// set download size
-		$headers[] = "Content-Length: " . filesize($filename);
-
-		// make sure headers have not been sent already
-		if(headers_sent()) {
-				$response['type'] = 'error: headers already sent';
-				$response = json_encode($response);
-				die;
-		}
-
-		// send headers
-		foreach($headers as $header) {
-				header($header . "\r\n");
-		}
-
-		// disable compression for the duration of file download
-		if(ini_get('zlib.output_compression')){
-				ini_set('zlib.output_compression', 'Off');
-		}
-
-		// read the file to output - if using on flywheel site, use readfile instead
-		if(function_exists('fpassthru')) {
-				$fp = fopen($filename, 'rb');
-				fpassthru($fp);
-				fclose($fp);
-		} else {
-				readfile($filename);
-		}
-
-		// remove temp file
-		unlink($filename);
-
-		// exit
-		exit;
-}
-
-/*
- * generate & download CSV with reports data
-*/
-add_action('wp_ajax_ta_download_report_csv','ta_download_report_csv');
-function ta_download_report_csv() {
-	global $ta_report_csv_headers;
-	global $current_user;
-
-	// set headers
-	$headers = array();
-	$headers[] = "Content-Disposition: attachment; filename=\"thinkeracademy_student_report.csv\"";
-	$headers[] = "Content-Type: text/csv";
-	$headers[] = "Cache-Control: max-age=0, no-cache, no-store";
-	$headers[] = "Pragma: no-cache";
-	$headers[] = "Connection: close";
-
-	// set default csv headers
-	$csv_headers = implode(',',$ta_report_csv_headers) . "\n";
-
-	// generate user data
-	$user = $current_user;
-	$user_id = $user->ID;
-	$course_id = learndash_get_last_active_course($user_id);
-	if(!$course_id) {
-		$course_id = 4192;
-	}
-	$users = ta_reports_get_user_data($user,$user_id,$course_id);
-	$csv_rows = "";
-	// loop over user data & generate csv row for each
-	foreach($users as $user) {
-		$user_row = '';
-
-		// loop over each cell
-		foreach($user as $cell) {
-			if(!is_array($cell)) {
-				// escape any commas and append
-				$user_row .= str_replace(',','',$cell) . ',';
-			} else {
-				// reverse scores
-				// ksort($cell);
-				// flatten the scores array and append
-				foreach($cell as $score_attempts) {
-					$user_row .= implode(',',$score_attempts) . ',';
-				}
-			}
-		}
-
-		$csv_rows .= substr_replace($user_row,"\n",-1);	// replace the last comma with a \n
-	}
-
-	// create temp dir/file
-	$tmp_dir = sys_get_temp_dir();
-	$filename = tempnam( $tmp_dir, 'tarep_data_');
-
-	// open file for appending
-	$csv_file = fopen($filename, 'a');
-
-	// write csv header, rows to file
-	fprintf($csv_file, '%s', $csv_headers);
-	fprintf($csv_file, '%s', $csv_rows);
-
-	// send data
-	ta_send_report_data($csv_file,$filename,$headers);
-
-	exit;
-}
 
 ?>
